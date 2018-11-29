a463369954699b656996b1984b6f7fd6f324856f
kuehma02 <kuehma02@luther.edu>
2018-11-29 14:16:41 -0600

    updated stuff

--- final_project.py
+++ final_project.py
@@ -2,24 +2,23 @@ from flask import Flask
 from flask import redirect, url_for
 from flask import request, make_response, Response
 from flask import send_from_directory, jsonify
+from flask import render_template
 import json
 import os
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     if request.method == "GET":
         return render_template("index.html")
     if request.form.get("addRow"):
-        pass cuz idk what im doing
+        pass
 
-@app.route('/')
+@app.route('/classes')
 def classes():
     all_classes = get_data_from_db("select code, name from classes;")
     
-
-def 
 
 if __name__ == '__main__':
     app.run()
