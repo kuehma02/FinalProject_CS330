9a230ce8045a305fe690a81fbd4ebdc3dffc4d70
kuehma02 <kuehma02@luther.edu>
2018-11-29 14:02:56 -0600

    started routes

--- final_project.py
+++ final_project.py
@@ -9,7 +9,15 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    pass
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.form.get("addRow"):
+        pass cuz idk what im doing
+
+@app.route('/')
+def classes():
+    all_classes = get_data_from_db("select code, name from classes;")
+    
 
 if __name__ == '__main__':
     app.run()
