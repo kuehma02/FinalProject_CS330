a02499f8e17e72a2dda0be6ee86313012c66600f
Anna Peterson <petean09@luther.edu>
2018-11-30 14:07:39 -0600

    final

--- db_setup.py
+++ db_setup.py
@@ -2,13 +2,37 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+
+# class Config(object):
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///planner.db?check_same_thread=False'
+
+
+# db = SQLAlchemy()
+
+
+# def create_app():
+#     app.config.from_object(Config)
+#     app = Flask(__name__)
+#     db.init_app(app)
+
+# engine = create_engine('sqlite:///planner.db?check_same_thread=False')
+
 engine = create_engine('sqlite:///planner.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+                                        autoflush=False,
+                                        bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db():
     import models
     Base.metadata.create_all(bind=engine)
+
+
+
--- main.py
+++ main.py
@@ -7,13 +7,30 @@ from flask import redirect, url_for, request, make_response, Response
 from flask import send_from_directory, jsonify, render_template
 
 from models import Class, Assignment, Grade
- 
+
 init_db()
 
 #Class.__table__.drop(engine)
 #new_class = Class('CS330', "Internet Programming", "Roman Yasinovskyy", "TH 12:45-2:15")
 #We should create a txt file that has a list of these objects
+
+
+
+db_list = []
+with open('classfile', 'r') as file:
+    for line in file:
+        course_lst = line.strip().split(',')
+        db_list.append(course_lst)
+# print(db_list)
+
+
+for course in db_list:
+    # print(course[0])
+    new_class = Class(course[0], course[1], course[2], course[3])
+    db_session.add(new_class)
+
 db.create_all()
+
 #db_session.add(new_class)
 #res = db_session.query(Class).filter(Class.name=="Internet Programming").first()
 #db_session.delete(res)
