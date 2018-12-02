e919222493c699570c02afc3ba59113fa8bc11cb
kuehma02 <kuehma02@luther.edu>
2018-12-02 14:59:46 -0600

    final :

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planner.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = b'^\x97\x91S!\xc1\x1b\xce\x0b\x92\xd8\x15Q$\xfe\x9f'
