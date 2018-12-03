a824f499a66255b4af7e0126d6067c22fb37a766
kuehma02 <kuehma02@luther.edu>
2018-12-03 17:04:06 -0600

    Finished Product

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# test.py
- 
 from app import app, db
 from db_setup import init_db, db_session,engine
 
@@ -11,48 +9,14 @@ from models import Class, Assignment, Grade
 from sqlalchemy.sql import select
 
 import os
+import json
 
 init_db()
 
 #Class.__table__.drop(engine)
-#new_class = Class('CS330', "Internet Programming", "Roman Yasinovskyy", "TH 12:45-2:15")
-#We should create a txt file that has a list of these objects
 #Assignment.__table__.drop(engine)
 #Grade.__table__.drop(engine)
 db.create_all()
-
-db_list = []
-
-# with open('classfile', 'r') as file:
-#     for line in file:
-#         course_lst = line.strip().split(', ')
-#         db_list.append(course_lst)
-#     print(db_list)
-
-
-# for course in db_list:
-#     print(course[0])
-#     new_class = Class(course[0], course[1], course[2], course[3])
-#     db_session.add(new_class)
-
-# db_list2 = []
-# with open('assignments', 'r') as file:
-#     for line in file:
-#         a_list = line.strip().split(', ')
-#         db_list2.append(a_list)
-#     print(db_list2)
-
-
-# for assign in db_list2:
-#     print(assign[0])
-#     new_assign = Assignment(name=assign[0],class_id = assign[1])
-#     db_session.add(new_assign)
-#res = db_session.query(Class).filter(Class.name=="Internet Programming").first()
-#db_session.delete(res)
-#res = db_session.query(Class).all()
-#for artist in res:
-    #print(artist.name)
-
 db_session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -82,18 +46,22 @@ def search():
     if request.method == "GET":
         query = db_session.query(Class).filter_by(id=courseNum).first()
         courseInfo = [query.id, query.name, query.professor, query.time]
-    
         assignmentQuery = db_session.query(Assignment).filter_by(class_id = courseNum).all()
         all_assignments = []
+        totalPercent = 0
+        count = 0
         for row in assignmentQuery:
             assignment = row.name
-            print(assignment)
-            print(row.id)
             grade = db_session.query(Grade).filter_by(assignment_id=row.id).order_by(Grade.id.desc()).first()
-            print(grade.grade)
+            totalPercent += grade.grade
             all_assignments.append([assignment, grade.grade, row.id])
+            count += 1
+        if count == 0:
+            classGrade = 0
+        else:
+            classGrade = totalPercent / count
         db_session.commit()
-        return render_template('results.html', options = options, course = courseInfo, results = all_assignments)
+        return render_template('results.html', options = options, course = courseInfo, grade = "{:.2f}".format(classGrade), results = all_assignments)
      
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -105,11 +73,8 @@ def add():
     newAssignment =Assignment(name=name,class_id=course_num)
     db_session.add(newAssignment)
     db_session.commit()
-    print("newassignment id is")
-    print(newAssignment.id)
     db_session.add(Grade(grade=0, assignment_id = newAssignment.id))
     db_session.commit()
-
     return redirect(url_for('search', courseNum=course_num))
 
 @app.route('/delete', methods=['POST'])
@@ -119,7 +84,6 @@ def delete():
     deleteAssignment = db_session.query(Assignment).filter_by(name=assignmentName, class_id = courseNum).first()
     db_session.delete(deleteAssignment)
     db_session.commit()
-    
     return redirect(url_for('search', courseNum=courseNum))
 
 @app.route('/saveGrade', methods=['POST'])
@@ -130,26 +94,28 @@ def saveGrade():
     db_session.commit()
     return redirect(url_for('search', courseNum=request.form.get('course')))
 
+@app.route('/api/<courseName>')
+def getJSON(courseName):
+    all_assignments = db_session.query(Assignment).filter_by(class_id=courseName).all()
+    returnList = []
+    assignmentsList=[]
+    for assign in all_assignments:
+        assignmentObject = {}
+        assignmentObject['name'] = assign.name
+        assignmentObject['assignment_id'] = assign.id
+        grades = []
+        gradeQuery = db_session.query(Grade).filter_by(assignment_id=assign.id).order_by(Grade.id.desc()).all()
+        for grade in gradeQuery:
+            gradeObject = {}
+            gradeObject['grade_id'] = grade.id
+            gradeObject['percent'] = grade.grade
+            grades.append(gradeObject)
+        assignmentObject['grades'] = grades
+        returnList.append(assignmentObject)
+    apiDictionary = {}
+    apiDictionary['assignments'] = returnList
 
-'''I Think this is the kind of stuff that we have to do for the ajax stuff??'''
-
-# @app.route('/save', methods=['POST'])
-# def save():
-#     with open('file.json', 'wb') as outfile:
-#         outfile.write(request.data)
-#     return "Success"
-
-# @app.route('/get', methods=['GET'])
-# def get():
-#     if os.path.isfile('file.json'):
-#         with open('file.json', 'r') as infile:
-#             data = json.load(infile)
-#         response = jsonify(data)
-#         return response
-#     else:
-#         data = []
-#         response = jsonify(data)
-#         return response
+    return json.dumps(apiDictionary)
  
 if __name__ == '__main__':
     app.run()
